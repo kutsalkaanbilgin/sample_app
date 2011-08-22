9b5ebe50c2fcb59916fe22619baaa56c339a3a56
Kutsal Kaan Bilgin <kutsalbilgin@gmail.com>
2011-08-22 23:56:44 +0300

    latest commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if !(@title.nil?) then
+			base_title = base_title + " | " + @title
+		end
+		return base_title
+	end
+	
 end
